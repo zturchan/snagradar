3b37d8d64834898e2e233756e655ffa36519d866
Zak <zak.turchansky@gmail.com>
2025-04-08 21:48:09 -0700

    EVs cannot be valid without valid base stats

--- snagradar/pokemon.py
+++ snagradar/pokemon.py
@@ -23,13 +23,13 @@ class Pokemon:
     self.msg += msg + '\r\n'
 
   def evs_valid(self):
-    valid = (self.evs_hp is not None and self.evs_hp > 0 and self.evs_hp<= 252 and
-                self.evs_atk is not None and self.evs_atk > 0 and self.evs_atk<= 252 and
-                self.evs_defense is not None and self.evs_defense > 0 and self.evs_defense<= 252 and
-                self.evs_spatk is not None and self.evs_spatk > 0 and self.evs_spatk<= 252 and
-                self.evs_spdef is not None and self.evs_spdef > 0 and self.evs_spdef<= 252 and
-                self.evs_speed is not None and self.evs_speed > 0 and self.evs_speed<= 252
-                )
+    valid = (self.base_stats_valid() == True and
+             self.evs_hp is not None and self.evs_hp > 0 and self.evs_hp<= 252 and
+             self.evs_atk is not None and self.evs_atk > 0 and self.evs_atk<= 252 and
+             self.evs_defense is not None and self.evs_defense > 0 and self.evs_defense<= 252 and
+             self.evs_spatk is not None and self.evs_spatk > 0 and self.evs_spatk<= 252 and
+             self.evs_spdef is not None and self.evs_spdef > 0 and self.evs_spdef<= 252 and
+             self.evs_speed is not None and self.evs_speed > 0 and self.evs_speed<= 252)
     
     if(valid):
       return True
