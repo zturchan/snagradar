3ead688d4699561eb4632a274ce2d409728a0c41
Zak Turchansky <zak.turchansky@gmail.com>
2026-06-03 21:39:02 -0700

    Use fixed level adjusted speed values

--- snagradar/challenge.py
+++ snagradar/challenge.py
@@ -12,27 +12,27 @@ class Challenge:
 
     def determine_winner(self):
         # Return -1 if player underspeeds, 0 if sometimes, 1 if player always outspeeds
-        
-        player_speed = self.player_pokemon.base_speed + self.player_pokemon.speed_stat_points
+
+        player_speed = self.player_pokemon.speed_at_lvl_50(self.player_pokemon.speed_stat_points)
         if (self.player_pokemon.beneficial_nature):
             player_speed *= 1.1
         if (self.player_pokemon.tailwind):
             player_speed *= 2
-        
-        villain_min_speed = self.villain_pokemon.base_speed
+
+        villain_min_speed = self.villain_pokemon.speed_at_lvl_50(0)
         if (self.villain_pokemon.tailwind):
             villain_min_speed *= 2
-        
+
         if (player_speed < villain_min_speed):
             return -1
-        
+
         # Nature is assumed plus, since not available to player.
-        villain_max_speed = 1.1 * (self.villain_pokemon.base_speed + MAX_SPEED_STAT_POINTS)
-        
+        villain_max_speed = 1.1 * (self.villain_pokemon.speed_at_lvl_50(MAX_SPEED_STAT_POINTS))
+
         if (self.villain_pokemon.tailwind):
             villain_max_speed *=2
-            
+
         if (player_speed > villain_max_speed):
             return 1
-        
+
         return 0
